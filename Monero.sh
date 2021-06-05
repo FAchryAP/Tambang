a5d135d689195cf420f9328d0c893a8c3a29eaf1
FAchryAP <50170072+FAchryAP@users.noreply.github.com>
2021-06-05 08:33:05 +0700

    Update Monero.sh

--- Monero.sh
+++ Monero.sh
@@ -1,4 +1,5 @@
 apt sudo update
 wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-static-x64.tar.gz
 tar -xf xmrig-6.12.2-linux-static-x64.tar.gz
+cd xmrig-6.12.2
 ./xmrig -o sg.minexmr.com:443 -u 468S2GdVbT26r42jJrxWcKfHpZHkjLKMfTY5BSpEr71WFhMwj8ymtA5WpFr6ZSyyFeDR8fZpafq3VBXwDRseWGjETUKsHWg -k --tls
